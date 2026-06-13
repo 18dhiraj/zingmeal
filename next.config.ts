f4888d79ba18477165926083b743499fccbcacfd
Dhiraj <officialdhiraj00@gmail.com>
2026-06-13 15:51:04 +0530

    update/config

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'zingmeal.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
